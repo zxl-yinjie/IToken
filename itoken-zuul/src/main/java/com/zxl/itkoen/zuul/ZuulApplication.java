0db5488f0ffa3af562b6e06eb273d532157a4bd3
zxl <1782391567@qq.com>
2020-12-19 08:55:48 +0800

    更新

--- itoken-zuul/src/main/java/com/zxl/itkoen/zuul/ZuulApplication.java
+++ itoken-zuul/src/main/java/com/zxl/itkoen/zuul/ZuulApplication.java
@@ -1,5 +1,6 @@
 package com.zxl.itkoen.zuul;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,4 +11,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 @EnableDiscoveryClient
 public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class,args);
+    }
 }
